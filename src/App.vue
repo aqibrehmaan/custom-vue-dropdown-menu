7d631313e287b977c3bae2000ab033e79749ec3d
Aqib Rehman <aqibr777@gmail.com>
2022-01-18 00:22:09 +0500

    Fresh Component with list of items

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
     <div class="container mx-auto p-6"> 
-      <div class="relative">
-          <a href="" class="text-sm inline-flex bg-gray-300 h-10 px-4 rounded text-center flex items-center justify-center leading-none text-gray-600">Button</a>
-          <ul class="bg-white shadow-xl absolute rounded overflow-hidden z-50 w-40 mt-2">
-            <li><a href="" class="block p-2 text-sm text-gray-800 hover:bg-gray-100">Item one</a></li>
-             <li><a href="" class="block p-2 text-sm text-gray-800 hover:bg-gray-100">Item two</a></li>
-          </ul>
-      </div>
+        <AppDropdown :items="items" />
     </div>
 </template>
 
 <script>
 
+import AppDropdown from '@/components/dropdown/AppDropdown'
+
 export default {
-  
+    components: {
+      AppDropdown
+    },
+    data() {
+      return {
+        items: [
+          { title: 'Price: High to low', slug: 'price-desc'},
+          { title: 'Price: Low to high', slug: 'price-asc'}
+        ]
+      }
+    }
 }
 </script>
 
---
+++ src/components/dropdown/AppDropdown.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="relative">
+          <a href="" class="text-sm inline-flex bg-gray-300 h-10 px-4 rounded text-center flex items-center justify-center leading-none text-gray-600">Button</a>
+          <ul class="bg-white shadow-xl absolute rounded overflow-hidden z-50 w-40 mt-2">
+            <li v-for="(item, index) in items" :key="index"><a href="" class="block p-2 text-sm text-gray-800 hover:bg-gray-100">{{ item.title }}</a></li>
+          </ul>
+      </div>
+</template>
+
+<script>
+export default {
+    props: {
+        items: {
+            required: true,
+            type: Array
+        }
+    }
+}
+</script>
